f9a8428d3bbd8eefa2aadb4ea2f9a6c61dcfe416
Nanda Gundapaneni <nanda.gundapaneni@markit.com>
2015-11-18 10:49:21 -0700

    Initial Commit

---
+++ NGSuggestions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NGSuggestions
+//
+//  Created by Nanda Gundapaneni on 11/18/15.
+//  Copyright © 2015 NandaKG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
